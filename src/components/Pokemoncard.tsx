2c895158b70cfccf42cdacec38addc10b7ee4cbe
jimmy <jimmy.balay97@gmail.com>
2025-04-04 11:33:07 +0200

    finishquete4

--- src/components/Pokemoncard.tsx
+++ src/components/Pokemoncard.tsx
@@ -1,26 +1,17 @@
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
+function PokemonCard({ pokemon }) {
 
-function PokemonCard() {
 
-    const pokemon = pokemonList[1];
 
     return (
-        <figure>
+        <div>
             {pokemon.imgSrc !== undefined ? (
                 <img src={pokemon.imgSrc} alt={pokemon.name} />
             ) : (
                 <p>???</p>
             )}
-        </figure >
+
+            <figcaption>{pokemon.name}</figcaption>
+        </div >
 
     )
 }
