3ab4a7c66ff3d6d672b9ba38ca5438e8ef9c59d1
jimmy <jimmy.balay97@gmail.com>
2025-04-14 09:19:41 +0200

    quete7finnish

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,21 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
@@ -28,8 +43,11 @@ function App() {
     <>
       <div>
         <p> Choisis ton pokemon : {pokemonName}</p>
-        <button type="button" onClick={() => setPokemonName("bulbasaur")}>bulbasaur</button>
         <button type="button" onClick={() => setPokemonName("mew")}>mew</button>
+        <button type="button" onClick={() => setPokemonName("bulbasaur")}>bulbasaur</button>
+        <button type="button" onClick={() => setPokemonName("charmander")}>charmander</button>
+        <button type="button" onClick={() => setPokemonName("pikachu")}>pikachu</button>
+        <button type="button" onClick={() => setPokemonName("squirtle")}>squirtle</button>
         <PokemonCard pokemon={pokemon} />
 
       </div>
